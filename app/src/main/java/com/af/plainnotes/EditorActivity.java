41dcbf31765b67f13c464a45dd629b6776bf301e
Ahmed Fahmy <fahmy@Ahmeds-MacBook-Pro.local>
2018-11-25 16:25:53 +0200

    Initial commit

---
+++ app/src/main/java/com/af/plainnotes/EditorActivity.java
@@ -0,0 +1,125 @@
+package com.af.plainnotes;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private String mAction;
+    private String mNoteFilter;
+    private String mOldText;
+
+    @BindView(R.id.editText) EditText mEditor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+
+        Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
+        if(uri == null) {
+            mAction = Intent.ACTION_INSERT;
+            setTitle(R.string.new_note);
+        } else {
+            mAction = Intent.ACTION_EDIT;
+            mNoteFilter = DBOpenHelper.COLUMN_NOTE_ID + "=" + uri.getLastPathSegment();
+
+            Cursor cursor = getContentResolver().query(uri, DBOpenHelper.ALL_COLUMNS, mNoteFilter,
+                    null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+            mOldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.COLUMN_NOTE_TEXT));
+            mEditor.setText(mOldText);
+            mEditor.requestFocus();
+            cursor.close();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finishEditing();
+                break;
+            case R.id.action_delete:
+                deleteNote();
+                break;
+        }
+
+        return true;
+    }
+
+    private void deleteNote() {
+        getContentResolver().delete(NotesProvider.CONTENT_URI, mNoteFilter, null);
+        Toast.makeText(this, R.string.note_deleted, Toast.LENGTH_LONG).show();
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if(mAction.equals(Intent.ACTION_EDIT)) {
+            getMenuInflater().inflate(R.menu.menu_editor, menu);
+        }
+        return true;
+    }
+
+    private void finishEditing() {
+        String newText = mEditor.getText().toString().trim();
+        switch (mAction) {
+            case Intent.ACTION_INSERT:
+                if(newText.length() == 0) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    insertNote(newText);
+                }
+                break;
+            case Intent.ACTION_EDIT:
+                if(newText.length() == 0) {
+                    deleteNote();
+                } else if(mOldText.equals(newText)) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    updateNote(newText);
+                }
+        }
+        finish();
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.COLUMN_NOTE_TEXT, noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI, values, mNoteFilter, null);
+        Toast.makeText(this, R.string.note_updated, Toast.LENGTH_LONG).show();
+        setResult(RESULT_OK);
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.COLUMN_NOTE_TEXT, noteText);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishEditing();
+    }
+}
---
+++ app/src/main/java/com/af/plainnotes/MainActivity.java
@@ -0,0 +1,172 @@
+package com.af.plainnotes;
+
+import android.app.AlertDialog;
+import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.github.fabtransitionactivity.SheetLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>, SheetLayout.OnFabAnimationEndListener {
+
+    private static final int EDITOR_REQUEST_CODE = 1001;
+
+    private CursorAdapter mCursorAdapter;
+
+    @BindView(android.R.id.list) ListView mListView;
+    @BindView(R.id.bottom_sheet) SheetLayout mSheetLayout;
+    @BindView(R.id.fab) FloatingActionButton mFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mCursorAdapter = new NotesCursorAdapter(this, null, 0);
+
+        mListView.setAdapter(mCursorAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Uri uri = Uri.parse(NotesProvider.CONTENT_URI + "/" + id);
+                intent.putExtra(NotesProvider.CONTENT_ITEM_TYPE, uri);
+                startActivityForResult(intent, EDITOR_REQUEST_CODE);
+            }
+        });
+
+        getLoaderManager().initLoader(0, null, this);
+
+        mSheetLayout.setFab(mFab);
+        mSheetLayout.setFabAnimationEndListener(this);
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.COLUMN_NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
+    }
+
+    private void deleteAllNotes() {
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //Insert Data management code here
+
+                            getContentResolver().delete(NotesProvider.CONTENT_URI, null, null);
+                            restartLoader();
+                            Toast.makeText(MainActivity.this, getString(R.string.all_deleted), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void insertSampleData() {
+        insertNote("Simple note");
+        insertNote("Multi-line\nnote");
+        insertNote("Ver long note with a lot of text that exceeds the width of the screen");
+        restartLoader();
+    }
+
+    @OnClick(R.id.fab)
+    void onFabClick() {
+        mSheetLayout.expandFab();
+    }
+
+    @Override
+    public void onFabAnimationEnd() {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivityForResult(intent, EDITOR_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EDITOR_REQUEST_CODE) {
+            mSheetLayout.contractFab();
+        }
+
+        if (requestCode == EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
+            restartLoader();
+        }
+    }
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0, null, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        mCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+    }
+}
